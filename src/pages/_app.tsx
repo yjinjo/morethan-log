861d5956f09af33acc2c930390653325e4462fb1
Youngjin Jo <yjinjo@berkeley.edu>
2025-04-21 23:18:16 +0900

    add vercel speed-insights

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { AppPropsWithLayout } from "../types"
 import { Hydrate, QueryClientProvider } from "@tanstack/react-query"
 import { RootLayout } from "src/layouts"
 import { queryClient } from "src/libs/react-query"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout || ((page) => page)
@@ -9,7 +10,10 @@ function App({ Component, pageProps }: AppPropsWithLayout) {
   return (
     <QueryClientProvider client={queryClient}>
       <Hydrate state={pageProps.dehydratedState}>
-        <RootLayout>{getLayout(<Component {...pageProps} />)}</RootLayout>
+        <RootLayout>
+          {getLayout(<Component {...pageProps} />)}
+          <SpeedInsights />
+        </RootLayout>
       </Hydrate>
     </QueryClientProvider>
   )
